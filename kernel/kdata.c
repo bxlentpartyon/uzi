b6c4bfd8230876897629716fb3605b6da665f4b3
Alex Thorlton <bxlentpartyon@gmail.com>
2023-02-02 23:31:55 -0600

    kdata.c: Re-organize shared data file.

--- kernel/kdata.c
+++ kernel/kdata.c
@@ -1,12 +1,19 @@
 #include <extern.h>
 
+/* Process/userspace stuff */
+
 struct u_data udata;
 struct p_tab ptab[PTABSIZE];
 
-inoptr root;   /* Address of root dir in inode table */
-
-struct cinode i_tab[ITABSIZE];    /* In-core inode table */
+/* Interrupt/timer stuff */
 
 time_t tod;      /* Time of day */
-
 int16 inint;   /* flag is set whenever interrupts are being serviced */
+
+/* Filesystem stuff */
+
+inoptr root;   /* Address of root dir in inode table */
+struct cinode i_tab[ITABSIZE];    /* In-core inode table */
+
+/* Driver stuff */
+
